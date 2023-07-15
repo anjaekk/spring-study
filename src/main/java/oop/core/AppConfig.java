1625ff248e2b4c49425b007deb5d0ba56625530c
anjaekk <anjaekk@gmail.com>
2023-07-15 19:57:52 +0900

    AppConfig 리팩토링

--- src/main/java/oop/core/AppConfig.java
+++ src/main/java/oop/core/AppConfig.java
@@ -1,6 +1,8 @@
 package oop.core;
 
+import oop.core.discount.DiscountPolicy;
 import oop.core.discount.FixDiscountPolicy;
+import oop.core.member.MemberRepository;
 import oop.core.member.MemberService;
 import oop.core.member.MemberServiceImpl;
 import oop.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import oop.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    private MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
